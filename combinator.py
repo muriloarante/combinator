9aeb0745124fee6102c79154d40b56212b47a4cf
murilo <arantemurilo@gmail.com>
2024-10-20 14:17:34 -0300

    Create combinator.py

---
+++ combinator.py
@@ -0,0 +1,14 @@
+from itertools import combinations
+
+def printCombinations(arr, size):
+    for p in combinations(arr, size):
+        for c in p:
+            print(c, end=" ")
+        print("")
+    
+size = 1 # Quantos elementos a lista deve ter.
+chars = [c for c in range(1, size + 1)]
+cSize = 1 # Quantos elementos cada combinação deve ter.
+
+printCombinations(chars, cSize)
+print(f"São {len(list(combinations(chars, cSize)))} listas possíveis.")
